b4565ee6d6c06a63698544c799ff58eb2673cd49
olha.codes <57961694+olhacodes@users.noreply.github.com>
2022-11-02 14:44:09 +0100

    added add product btn and edite property

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -11,6 +11,8 @@ import { ProductsService } from 'src/app/services/products.service';
 export class ProductsComponent implements OnInit {
   products: IProducts[];
   productsSubscription: Subscription;
+  canEdit: true;
+  canView: boolean = false;
 
   constructor(private productsService: ProductsService) { }
 
@@ -18,10 +20,12 @@ export class ProductsComponent implements OnInit {
     this.productsSubscription = this.productsService.getProducts().subscribe(data => {
       this.products = data;
     })
+
+    this.canEdit = true;
   }
 
   ngOnDestroy() {
-    if(this.productsSubscription) this.productsSubscription.unsubscribe(); 
+    if(this.productsSubscription) this.productsSubscription.unsubscribe();
   }
 
 }

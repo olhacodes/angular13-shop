007f073d04f6fdd675677c98b8f4b03cc9b7571c
olha.codes <57961694+olhacodes@users.noreply.github.com>
2022-11-02 11:01:36 +0100

    created main files and folders

---
+++ src/app/components/busket/busket.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-busket',
+  templateUrl: './busket.component.html',
+  styleUrls: []
+})
+export class BusketComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/product-details/product-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: []
+})
+export class ProductDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
